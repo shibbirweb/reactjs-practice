3627dc6b3cd2797ff05e39b1c881f04d4d5940c0
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-04-17 21:24:53 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 //import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 import MyApp from "./components/MyApp";
 
-
 ReactDOM.render(
-    <React.StrictMode>
-        <MyApp message="From ES6 Component Props"/>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <MyApp message="From ES6 Component Props" />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
