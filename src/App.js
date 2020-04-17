6885607c8ea1251303cd8636f566e03a63cfa798
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-04-18 00:11:04 +0600

    Add Bootstrap and Read Task List

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import logo from "./logo.svg";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tasks: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:4000/tasks")
+      .then((res) => res.json())
+      .then((tasks) => this.setState({ tasks }))
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    const tasks = this.state.tasks.map((task) => (
+      <li key={task.id} className="list-group-item">
+        {task.title} - {task.isCompleted} -{" "}
+        <button className="btn btn-primary btn-sm">Edit</button>
+      </li>
+    ));
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <ul className="list-group">
+                <li className="list-group-item active">Tasks</li>
+                {tasks}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
