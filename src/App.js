fbc7e3c498ac0f38dc9ac624c71cdb6defc87121
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-05-13 14:26:46 +0600

    Delete Complete

--- src/App.js
+++ src/App.js
@@ -5,59 +5,80 @@ import logo from "./logo.svg";
 import AddForm from "./components/AddForm"
 
 class App extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      tasks: [],
-    };
+        this.state = {
+            tasks: [],
+        };
 
-    this.addRecord = this.addRecord.bind(this)
-  }
+        this.addRecord = this.addRecord.bind(this)
+        this.delRecord = this.delRecord.bind(this)
+    }
 
-  componentDidMount() {
-    fetch("http://localhost:4000/tasks")
-      .then((res) => res.json())
-      .then((tasks) => this.setState({ tasks }))
-      .catch((error) => console.log(error));
-  }
+    componentDidMount() {
+        this.fetchData()
+    }
 
-  addRecord(e){
-      /*const tasks = this.state.tasks;
-      tasks.push(e)*/
-      const tasks = [...this.state.tasks, e]
-      this.setState({
-          tasks: tasks
-      })
-  }
+    fetchData() {
+        fetch("http://localhost:4000/tasks")
+            .then((res) => res.json())
+            .then((tasks) => this.setState({tasks}))
+            .catch((error) => console.log(error));
+    }
 
-  render() {
-    const tasks = this.state.tasks.map((task) => (
-      <li key={task.id} className="list-group-item">
-        {task.title} - {task.isCompleted} -{" "}
-        <button className="btn btn-primary btn-sm">Edit</button>
-      </li>
-    ));
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+    addRecord(e) {
+        /*const tasks = this.state.tasks;
+        tasks.push(e)*/
+        const tasks = [...this.state.tasks, e]
+        this.setState({
+            tasks: tasks
+        })
+    }
 
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-6 offset-md-3">
-                <AddForm addRec={this.addRecord}/>
-              <ul className="list-group">
-                <li className="list-group-item active">Tasks</li>
-                {tasks}
-              </ul>
+    delRecord(e) {
+        const id = e.target.getAttribute("data-key");
+        fetch(`http://localhost:4000/tasks/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(() => {
+                this.fetchData()
+            })
+            .catch(error => console.log(error))
+    }
+
+    render() {
+        const tasks = this.state.tasks.map((task) => (
+            <li key={task.id} className="list-group-item">
+                {task.title} - {task.isCompleted} -{" "}
+                <button className="btn btn-primary btn-sm">Edit</button>
+                <button className="btn btn-danger btn-sm ml-2" data-key={task.id} onClick={this.delRecord}>Delete
+                </button>
+            </li>
+        ));
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
+
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <AddForm addRec={this.addRecord}/>
+                            <ul className="list-group">
+                                <li className="list-group-item active">Tasks</li>
+                                {tasks}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
