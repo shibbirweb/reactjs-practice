91991019b4ea07e412d5ed89e20016895560440c
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-05-20 17:53:48 +0600

    Update style

--- src/components/TasksComponent.js
+++ src/components/TasksComponent.js
@@ -7,11 +7,30 @@ class TasksComponent extends Component {
                 <div className="col-12">
                     <h5 className="text-center">Tasks</h5>
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Cras justo odio</li>
-                        <li className="list-group-item">Dapibus ac facilisis in</li>
-                        <li className="list-group-item">Morbi leo risus</li>
-                        <li className="list-group-item">Porta ac consectetur ac</li>
-                        <li className="list-group-item">Vestibulum at eros</li>
+                        <li className="list-group-item">
+                            <input className="form-check-input" type="checkbox"/>
+                            Cras justo odio
+                        </li>
+                        <li className="list-group-item">
+                            <input className="form-check-input" type="checkbox" defaultChecked={true}/>
+                            <s>Dapibus ac facilisis in</s>
+                        </li>
+                        <li className="list-group-item">
+                            <input className="form-check-input" type="checkbox"/>
+                            Morbi leo risus
+                        </li>
+                        <li className="list-group-item">
+                            <input className="form-check-input" type="checkbox"/>
+                            Porta ac consectetur ac
+                        </li>
+                        <li className="list-group-item">
+                            <input className="form-check-input" type="checkbox" disabled={true}/>
+                            <form className="form-inline">
+                                <input type="text" className="form-control form-control-sm"
+                                       defaultValue="Vestibulum at eros"
+                                       placeholder="Jane Doe"/>
+                            </form>
+                        </li>
                     </ul>
                 </div>
             </div>
