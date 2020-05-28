7783e0f7b13d169041166fa0e6c9fec24219f266
Shibbir Ahmed <shibbirweb@gmail.com>
2020-05-29 03:10:36 +0600

    Add jQuery, pooper.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery'
+import 'popper.js'
+import 'bootstrap/dist/js/bootstrap'
 import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/Navbar";
+import Navbar from "./components/layouts/Navbar";
 
 function App() {
   return (
