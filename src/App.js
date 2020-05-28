e273f2ebbdc9200cd6ee9952ae34e4d14666981b
Shibbir Ahmed <shibbirweb@gmail.com>
2020-05-29 03:00:21 +0600

    install bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
