e6a238d53a4cc51f145c90347e10d2d161063072
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-07-10 00:47:59 +0600

    4. Comparing Stateless and Stateful Components

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Person from "./Person/Person"
+
+
+const Persons = (props) => props.persons.map((person, index) => {
+        return <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            click={() => props.clicked(index)}
+            changed={(event) => props.changed(event, person.id)}
+        />
+
+    });
+
+
+export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './App.module.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 
 
@@ -56,51 +57,26 @@ class App extends Component {
     render() {
 
         let persons = null;
-        let btnClass = '';
+        
 
         if (this.state.showPersons) {
             persons = (
-                <div>
-                    {
-                        this.state.persons.map((person, index) => {
-                            return <Person
-                                    key={person.id}
-                                    name={person.name}
-                                    age={person.age}
-                                    click={() => this.deletePersonHandler(index)}
-                                    changed={(event) => this.nameChangeHandler(event, person.id)}
-                                />
-                            
-                        })
-                    }
-                </div>
+                <Persons 
+                        persons={this.state.persons}
+                        clicked={ this.deletePersonHandler}
+                        changed={this.nameChangeHandler}
+                />
             )
-
-
-            btnClass = classes.Red;
         }
 
-        //let classes = ['red', 'bold'].join(' ');
-        const assignedClasses = [];
-
-        if (this.state.persons.length <= 2){
-            assignedClasses.push(classes.red) // classes = ['red']
-        }
-        if (this.state.persons.length <= 1){
-            assignedClasses.push(classes.bold) // classes = ['red bold']
-        }
-
-
+        
         return (
                 <div className={classes.App}>
-                    <h1>Hi, I'm React App</h1>
-                    <p className={assignedClasses.join(' ')}>This is really working</p>
-
-                    <button className={btnClass}
-                        onClick={this.togglePersonsHandler}
-
-                    >Switch Name
-                    </button>
+                    <Cockpit 
+                        showPersons={this.state.showPersons}
+                        persons ={this.state.persons}
+                        clicked={this.togglePersonsHandler}
+                    />
 
                     {persons}
                 </div>
