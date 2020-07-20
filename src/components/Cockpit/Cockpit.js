03f09a18d57a7023d3022d167476a31dcd382f08
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-07-21 00:24:40 +0600

    15. Optimizing Functional Components with React.memo()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -36,10 +36,10 @@ const Cockpit = (props) => {
 
 
 
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
         assignedClasses.push(classes.red) // classes = ['red']
     }
-    if (props.persons.length <= 1) {
+    if (props.personsLength <= 1) {
         assignedClasses.push(classes.bold) // classes = ['red bold']
     }
 
@@ -57,4 +57,4 @@ const Cockpit = (props) => {
     )
 }
 
-export default Cockpit;
+export default React.memo(Cockpit); // optimization for every change update
