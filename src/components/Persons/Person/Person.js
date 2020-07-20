03710dd0e526304a2bfcbbfba125718ea55289b4
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-07-21 02:07:43 +0600

    22. Higher Order Components (HOC) - Introduction

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import classes from './Person.module.css';
 import Auxiliary from "../../../hoc/Auxiliary"
 
@@ -6,14 +6,14 @@ class Person extends Component {
     render() {
         console.log('[Person.js] is render')
         return (
-            <Fragment>
+            <Auxiliary>
                 <p key={"i1"}
                     onClick={this.props.click}> I
                     'm a {this.props.name} and I am {this.props.age} years old</p>
 
                 <p key={"i2"}> {this.props.children}</p>
                 <input key={"i3"} type="text" onChange={this.props.changed} value={this.props.name}/>
-            </Fragment>
+            </Auxiliary>
         )
     }
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-
+import WithClass from "../hoc/WithClass"
 
 
 class App extends Component {
@@ -101,7 +101,7 @@ class App extends Component {
 
         
         return (
-                <div className={classes.App}>
+                <WithClass classes={classes.App}>
                     <button onClick={() => (this.setState({showCockpit: false}))}>Remove Cockpit</button>
                     { this.state.showCockpit ?
                     <Cockpit 
@@ -113,7 +113,7 @@ class App extends Component {
 
 
                     {persons}
-                </div>
+                </WithClass>
         )
     }
 
