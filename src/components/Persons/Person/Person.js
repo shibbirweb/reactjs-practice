79f0f18f85a7bb4ad9a20b35b379abdab98d6b18
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-07-21 01:05:50 +0600

    19. Rendering Adjacent JSX Elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import classes from './Person.module.css';
-
+import Auxiliary from "../../../hoc/Auxiliary"
 
 class Person extends Component {
     render() {
         console.log('[Person.js] is render')
         return (
-            <div className={classes.Person}>
-                <p
+            <Auxiliary>
+                <p key={"i1"}
                     onClick={this.props.click}> I
                     'm a {this.props.name} and I am {this.props.age} years old</p>
 
-                <p> {this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
+                <p key={"i2"}> {this.props.children}</p>
+                <input key={"i3"} type="text" onChange={this.props.changed} value={this.props.name}/>
+            </Auxiliary>
         )
     }
 }
