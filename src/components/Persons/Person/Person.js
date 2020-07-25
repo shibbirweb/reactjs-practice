6b2e0d626f04212839170ec0fa7bbf0b99b865fb
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-07-26 00:01:48 +0600

    26. Using PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react'
-import classes from './Person.module.css';
-import Auxiliary from "../../../hoc/Auxiliary"
-import WithClass from '../../../hoc/withClass';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classes from "./Person.module.css";
+import Auxiliary from "../../../hoc/Auxiliary";
+import WithClass from "../../../hoc/withClass";
 
 class Person extends Component {
-    render() {
-        console.log('[Person.js] is render')
-        return (
-            <Auxiliary>
-                <p key={"i1"}
-                    onClick={this.props.click}> I
-                    'm a {this.props.name} and I am {this.props.age} years old</p>
+  render() {
+    console.log("[Person.js] is render");
+    return (
+      <Auxiliary>
+        <p key={"i1"} onClick={this.props.click}>
+          {" "}
+          I 'm a {this.props.name} and I am {this.props.age} years old
+        </p>
 
-                <p key={"i2"}> {this.props.children}</p>
-                <input key={"i3"} type="text" onChange={this.props.changed} value={this.props.name}/>
-            </Auxiliary>
-        )
-    }
+        <p key={"i2"}> {this.props.children}</p>
+        <input
+          key={"i3"}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Auxiliary>
+    );
+  }
 }
+
+// props validation
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
+};
 export default WithClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,127 +1,116 @@
-import React, {Component} from 'react';
-import classes from './App.module.css';
-import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from "../hoc/withClass"
-import Auxiliary from "../hoc/Auxiliary"
-
+import React, { Component } from "react";
+import classes from "./App.module.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+import WithClass from "../hoc/withClass";
+import Auxiliary from "../hoc/Auxiliary";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
 
-    constructor(props){
-        super(props);
-        console.log('[App.js] constructor');        
+  state = {
+    persons: [
+      { id: "i-1", name: "Shibbir", age: 26 },
+      { id: "i-2", name: "Ahmed", age: 28 },
+      { id: "i-3", name: "Raihan", age: 27 },
+    ],
+    showPersons: false,
+    showCockpit: true,
+    changeCounter: 0,
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+
+    return state;
+  }
+
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[App.js] componentDidUpdate");
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log("[App.js] shouldComponentUpdate");
+    return true;
+  }
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(
+      (_person) => _person.id === id
+    );
+
+    //const person = Object.assign({}, this.state.persons[personIndex])
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1,
+      };
+    });
+  };
+
+  deletePersonHandler = (index) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+
+    persons.splice(index, 1);
+
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    console.log("[App.js] render");
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangeHandler}
+        />
+      );
     }
 
+    return (
+      <Auxiliary classes={classes.App}>
+        <button onClick={() => this.setState({ showCockpit: false })}>
+          Remove Cockpit
+        </button>
+        {this.state.showCockpit ? (
+          <Cockpit
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            personsLength={this.state.persons.length}
+            clicked={this.togglePersonsHandler}
+          />
+        ) : null}
 
-    state = {
-        persons: [
-            {id: "i-1", name: "Shibbir", age: 26},
-            {id: "i-2", name: "Ahmed", age: 28},
-            {id: "i-3", name: "Raihan", age: 27}
-        ],
-        showPersons: false,
-        showCockpit: true,
-        changeCounter: 0
-    }
-
-    static getDerivedStateFromProps(props, state){
-        
-        console.log('[App.js] getDerivedStateFromProps', props);
-        
-        return state;
-    }
-
-    componentDidMount(){
-        console.log('[App.js] componentDidMount');
-        
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("[App.js] componentDidUpdate")
-    }
-
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        console.log("[App.js] shouldComponentUpdate")
-        return true;
-    }
-
-
-    nameChangeHandler = (event, id) => {
-
-        const personIndex = this.state.persons.findIndex(_person => _person.id === id);
-
-        //const person = Object.assign({}, this.state.persons[personIndex])
-        const person = {
-            ...this.state.persons[personIndex]
-        }
-
-        person.name = event.target.value;
-
-        const persons = [...this.state.persons];
-
-        persons[personIndex] = person;
-
-
-        this.setState((prevState, props) => {
-            return {
-                persons: persons,
-                changeCounter: prevState.changeCounter + 1
-            }
-        })
-    }
-
-    deletePersonHandler = (index) => {
-
-        //const persons = this.state.persons.slice();
-        const persons = [...this.state.persons];
-
-        persons.splice(index, 1);
-
-        this.setState({persons: persons})
-    }
-
-    togglePersonsHandler = () => {
-        const doesShow = this.state.showPersons;
-        this.setState({showPersons: !doesShow});
-    }
-
-
-    render() {
-
-        console.log('[App.js] render')
-
-        let persons = null;
-        
-
-        if (this.state.showPersons) {
-            persons = (
-                <Persons 
-                        persons={this.state.persons}
-                        clicked={ this.deletePersonHandler}
-                        changed={this.nameChangeHandler}
-                />
-            )
-        }
-
-        
-        return (
-            <Auxiliary classes={classes.App}>
-                    <button onClick={() => (this.setState({showCockpit: false}))}>Remove Cockpit</button>
-                    { this.state.showCockpit ?
-                    <Cockpit 
-                        title={this.props.appTitle}
-                        showPersons={this.state.showPersons}
-                        personsLength ={this.state.persons.length}
-                        clicked={this.togglePersonsHandler}
-                    /> : null }
-
-
-                    {persons}
-            </Auxiliary>
-        )
-    }
-
+        {persons}
+      </Auxiliary>
+    );
+  }
 }
 export default WithClass(App, classes.App);
-
