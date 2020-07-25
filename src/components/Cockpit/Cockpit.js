a12eb2e955b760ebacd9b4ccd263e41574eceb67
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-07-26 01:09:48 +0600

    31. contextType & useContext()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useContext} from 'react';
 import classes from './Cockpit.module.css'
 import AuthContext from "../../context/auth-context"
 
@@ -6,6 +6,10 @@ import AuthContext from "../../context/auth-context"
 const Cockpit = (props) => {
 
     const toggleButtonRef = useRef(null);
+
+    const authContext = useContext(AuthContext)
+
+    console.log(authContext)
 
     useEffect(() => {
         console.log("[Cockpit.js] useEffect")
@@ -62,9 +66,8 @@ const Cockpit = (props) => {
         >
           Switch Name
         </button>
-        <AuthContext.Consumer>
-          {(context) => <button onClick={context.login}>Log in</button>}
-        </AuthContext.Consumer>
+        
+        <button onClick={authContext.login}>Log in</button>
       </div>
     );
 }
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,20 +11,21 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext
+
   componentDidMount() {
     //this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated)
   }
 
   render() {
     console.log("[Person.js] is render");
     return (
       <Auxiliary>
-      <AuthContext.Consumer>
-        { (context) => 
-          context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
-        }
-      </AuthContext.Consumer>
+      
+        {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p> }
+        
 
         <p key={"i1"} onClick={this.props.click}>
           I 'm a {this.props.name} and I am {this.props.age} years old
