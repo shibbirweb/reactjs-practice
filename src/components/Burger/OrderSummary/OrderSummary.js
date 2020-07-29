66b1414953d0070ffabafdcd1dd3327e97753aa5
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-07-29 17:58:47 +0600

    21. Creating the Order Summary Modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Auxiliary from "../../../hoc/Auxiliary"
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (<li key={igKey}>
+                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: { props.ingredients[igKey]}
+            </li>)
+        });
+
+    return (
+        <Auxiliary>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Auxiliary>
+    )
+}
+
+export default orderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import Auxiliary from "../../hoc/Auxiliary"
 import Burger from "../../components/Burger/Burger"
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import Modal from "../../components/UI/Modal/Modal"
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -100,6 +102,9 @@ class BurgerBuilder extends Component{
 
        return (
            <Auxiliary>
+            <Modal>
+                   <OrderSummary ingredients={this.state.ingredients}/>
+            </Modal>
             <Burger ingredients={this.state.ingredients}/>
             <BuildControls 
                 ingredientAdded={this.addIngredientHandler}
