5f37f3b26a701e6bb12da478d06ae9d68875fa1e
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-08-05 16:32:10 +0600

    5. Sending a POST Request

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: "https://react-my-burger-78b06.firebaseio.com/",
+  baseURL: "https://react-my-burger-78b06.firebaseio.com",
 });
 
 export default instance
