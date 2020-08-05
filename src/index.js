0eb01cc5041ebdc3545121d535d364e0722acb2b
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-08-05 16:03:37 +0600

    15. Setting a Default Global Configuration for Axios

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios'
 
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common['Authorization'] = 'APP_TOKEN'
+axios.defaults.headers.common['Content-Type'] = 'application/json'
+
 axios.interceptors.request.use(request => {
     console.log(request);
     // Edit request config
