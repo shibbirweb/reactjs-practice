4a7be9877957918fe74cb7a90dadbf3749ca6f8f
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-08-05 13:24:18 +0600

    6. Transforming Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -13,14 +13,21 @@ class Blog extends Component {
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      this.setState({ posts: response.data });
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "Shibbir",
+        };
+      });
+      this.setState({ posts: updatedPosts });
       //console.log(response);
     });
   }
 
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} />;
+      return <Post key={post.id} title={post.title} author={post.author} />;
     });
 
     return (
