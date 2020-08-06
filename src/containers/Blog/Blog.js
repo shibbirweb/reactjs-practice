7adc1a1531df3b3c5564c34dd2a2bf1da79897e7
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-08-06 07:11:08 +0600

    7. Setting Up and Rendering Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 //import axios from "axios";
-import axios from "../../axios";
+import { Route } from "react-router-dom";
+
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 
@@ -20,7 +21,8 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home 2</h1>} />
       </div>
     );
   }
