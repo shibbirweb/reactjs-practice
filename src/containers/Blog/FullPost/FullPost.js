d6878fef7ce21f011a35f9d92053b921042fc2a1
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-08-06 07:50:42 +0600

    17. Extracting Route Parameters

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,13 +8,14 @@ class FullPost extends Component {
     loadedPost: null,
   };
 
-  componentDidUpdate() {
-    if (this.props.id) {
+  componentDidMount() {
+    console.log(this.props)
+    if (this.props.match.params.id) {
       if (
         !this.state.loadedPost ||
-        this.state.loadedPost.id !== this.props.id
+        this.state.loadedPost.id !== this.props.match.params.id
       ) {
-        axios.get("/posts/" + this.props.id).then((response) => {
+        axios.get("/posts/" + this.props.match.params.id).then((response) => {
           //console.log(response);
           this.setState({ loadedPost: response.data });
         });
@@ -23,7 +24,7 @@ class FullPost extends Component {
   }
 
   deletePostHandler = () => {
-    axios.delete("/posts/" + this.props.id).then((response) => {
+    axios.delete("/posts/" + this.props.match.params.id).then((response) => {
       console.log(response);
     });
   };
